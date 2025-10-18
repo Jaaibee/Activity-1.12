5e302b5d42d8aabfa2584b0e7ad635b3fcd31884
Jaaibee <houtaroujaybee@gmail.com>
2025-10-19 01:12:24 +0800

    Fix Vercel deployment paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: './',       // Make sure root points to project root where index.html is
   build: {
     outDir: 'dist'
   }
